297ad893ab5ed4cbd90b782d44f36d5dc3d05065
Jeff Pidcock <jpidcock@me.com>
2024-12-01 20:50:08 -0500

    Update README.md for personally entertaining design; add solution to Day 1 and initial Day 2 file.

--- days/day01/main.cpp
+++ days/day01/main.cpp
@@ -5,12 +5,13 @@ Part 1:
 Pair up the smallest number in the "left" list with the
 smallest number in the "right" list, and so on. Within
 each pair, figure out how far apart the two numbers are.
-The answer to Part 1  is the sum of all of these distances.
+The answer to Part 1 is the sum of all of these distances.
 
 Part 2:
-TBD
-
-
+For each number in the "left" list, find the number of times
+it appears in the "right" list. Multiply the number by the
+number of times it appears in the other list.
+The answer to Part 2 is the sum of all of these products.
 */
 
 #include <iostream>
@@ -34,19 +35,22 @@ int main() {
         left_nums.push_back(left);
     }
 
+    std::sort(left_nums.begin(), left_nums.end());
+    std::sort(right_nums.begin(), right_nums.end());
+
+    // Calculate the running sum of the absolute differences
+    for(int i = 0; i < 1000; i++){
+        running_sum += std::abs(right_nums.at(i) - left_nums.at(i));
+    }
     
+    // Calculate the similarity score by multiplying the number by the number of times it appears in the other list
     for(const auto& left_num : left_nums){
         int appearances = std::count(right_nums.begin(), right_nums.end(), left_num);
         similarity_score += left_num * appearances;
     }
 
-    std::sort(left_nums.begin(), left_nums.end());
-    std::sort(right_nums.begin(), right_nums.end());
-
-    for(int i = 0; i < 1000; i++){
-        running_sum += std::abs(right_nums.at(i) - left_nums.at(i));
-    }
-
     std::cout << "Final Distance:   " << running_sum << std::endl;
     std::cout << "Similarity Score: " << similarity_score << std::endl;
+
+    return 0;
 }
