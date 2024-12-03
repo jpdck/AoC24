142ab10f26401885a05f28bb25e8dd269a7066ec
Jeff Pidcock <jpidcock@me.com>
2024-12-02 22:31:38 -0500

    Final solution

--- days/day02/main.cpp
+++ days/day02/main.cpp
@@ -1,27 +1,62 @@
-/*
-Advent of Code: Day 2
-
-Part 1:
-For each line in the input, determine if the values are:
-- Increasing or decreasing in value
-- Has a difference in value between 1 and 3, inclusive
-
-*/
-
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <algorithm>
-#include <cstdlib>
 #include <sstream>
 
-bool isValidDiff(int a, int b)
+// Checks if differences between adjacent numbers are between 1 and 3
+bool hasValidDifferences(const std::vector<int> &nums)
 {
-    int diff = std::abs(a - b);
-    if (diff < 1 || diff > 3)
-        std::cout << "DIFF [" << std::to_string(diff) << "] [" << std::to_string(b) << "] [" << std::to_string(a) << "]" << std::endl;
+    for (size_t i = 1; i < nums.size(); i++)
+    {
+        int diff = std::abs(nums[i] - nums[i - 1]);
+        if (diff < 1 || diff > 3)
+            return false;
+    }
+    return true;
+}
 
-    return diff >= 1 && diff <= 3;
+// Checks if sequence is monotonic (all increasing or all decreasing)
+bool isMonotonic(const std::vector<int> &nums)
+{
+    if (nums.size() < 2)
+        return true;
+
+    bool increasing = nums[1] > nums[0];
+    for (size_t i = 2; i < nums.size(); i++)
+    {
+        if ((nums[i] > nums[i - 1]) != increasing)
+            return false;
+    }
+    return true;
+}
+
+// Checks if a sequence is safe according to basic rules
+bool isBasicSafe(const std::vector<int> &nums)
+{
+    return nums.size() >= 2 &&
+           hasValidDifferences(nums) &&
+           isMonotonic(nums);
+}
+
+// Checks if sequence can be made safe by removing one number
+bool isDampenerSafe(const std::vector<int> &nums)
+{
+    // First check if it's already safe without removing anything
+    if (isBasicSafe(nums))
+        return true;
+
+    // Try removing each number one at a time
+    for (size_t i = 0; i < nums.size(); i++)
+    {
+        std::vector<int> modified = nums;
+        modified.erase(modified.begin() + i);
+
+        // If removing this number makes the sequence safe, then it's dampener safe
+        if (isBasicSafe(modified))
+            return true;
+    }
+
+    return false;
 }
 
 int main()
@@ -35,43 +70,19 @@ int main()
         std::vector<int> nums;
         std::istringstream iss(line);
         int num;
-        int direction = 0;
-        bool valid = true;
 
         while (iss >> num)
-            nums.push_back(num);
-
-        if (nums.size() < 2)
-            continue;
-
-        for (size_t i = 1; i < nums.size() && valid; i++)
         {
-            valid = isValidDiff(nums.at(i), nums.at(i - 1));
-
-            if (!valid)
-                break;
-                
-            if (direction == 0)
-                direction = nums.at(i) > nums.at(i - 1) ? 1 : 2;
-
-            else if (direction == 1 && nums.at(i) < nums.at(i - 1))
-            {
-                std::cout << "CDIR [+1 -1] [" + std::to_string(nums.at(i - 1)) + "] [" + std::to_string(nums.at(i)) + "] [" << line << "]" << std::endl;
-                valid = false;
-                break;
-            }
-            else if (direction == 2 && nums.at(i) > nums.at(i - 1))
-            {
-                std::cout << "CDIR [-1 +1] [" + std::to_string(nums.at(i - 1)) + "] [" + std::to_string(nums.at(i)) + "] [" << line << "]" << std::endl;
-                valid = false;
-                break;
-            }
+            nums.push_back(num);
         }
 
-        safe_reports += valid;
+        // Using isDampenerSafe for Part 2
+        if (isDampenerSafe(nums))
+        {
+            safe_reports++;
+        }
     }
 
-    std::cout << "Safe Reports: " << safe_reports << std::endl;
-
+    std::cout << "Safe reports: " << safe_reports << std::endl;
     return 0;
 }
