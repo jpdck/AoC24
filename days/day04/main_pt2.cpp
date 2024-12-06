52828a36b46a18b0bf8367ec13e3a87db66c1465
Jeff Pidcock <jpidcock@me.com>
2024-12-06 17:37:50 -0500

    Implement Day 4, part 2 solution: count 'MAS' patterns in grid with directional checks

--- days/day04/main_pt2.cpp
+++ days/day04/main_pt2.cpp
@@ -25,8 +25,10 @@ int main()
     std::vector<std::vector<char>> grid;
     int count = 0;
 
-    while (getline(file, line)) {
-        if (!line.empty()) {
+    while (getline(file, line))
+    {
+        if (!line.empty())
+        {
             std::vector<char> row(line.begin(), line.end());
             grid.push_back(row);
         }
@@ -34,5 +36,127 @@ int main()
 
     file.close();
 
+    // loop through the grid and check:
+    // 1. If and where we are out of bounds
+    // 2. If we have a "MAS" pattern
+
+    for (int i = 0; i < grid.size(); i++)
+    {
+        for (int j = 0; j < grid[i].size(); j++)
+        {
+            // Do we have an M?
+            if (grid[i][j] != 'M')
+            {
+                continue;
+            }
+
+            // Check if we have a matching M in one of the four directions
+            // 1. Right
+            if (j + 2 < grid[i].size() && grid[i][j + 2] == 'M')
+            {
+                // Check if we have an A below
+                if (i + 1 < grid.size() && grid[i + 1][j + 1] == 'A')
+                {
+                    // Check if we have an S below
+                    if ((i + 2 < grid.size() && grid[i + 2][j] == 'S') &&
+                        (i + 2 < grid.size() && grid[i + 2][j + 2] == 'S'))
+                    {
+                        count++;
+                    }
+                }
+                // Check if we have an A above
+                if (i - 1 >= 0 && grid[i - 1][j + 1] == 'A')
+                {
+                    // Check if we have an S above
+                    if ((i - 2 >= 0 && grid[i - 2][j] == 'S') &&
+                        (i - 2 >= 0 && grid[i - 2][j + 2] == 'S'))
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            // 2. Left
+            if (j - 2 >= 0 && grid[i][j - 2] == 'M')
+            {
+                // Check if we have an A above
+                if (i - 1 >= 0 && grid[i - 1][j - 1] == 'A')
+                {
+                    // Check if we have an S above
+                    if ((i - 2 >= 0 && grid[i - 2][j] == 'S') &&
+                        (i - 2 >= 0 && grid[i - 2][j - 2] == 'S'))
+                    {
+                        count++;
+                    }
+                }
+
+                // Check if we have an A below
+                if (i + 1 < grid.size() && grid[i + 1][j - 1] == 'A')
+                {
+                    // Check if we have an S above
+                    if ((i + 2 < grid.size() && grid[i + 2][j] == 'S') &&
+                        (i + 2 < grid.size() && grid[i + 2][j - 2] == 'S'))
+                    {
+                        count++;
+                    }
+                }
+
+            }
+        
+            // 3. Down
+            if (i + 2 < grid.size() && grid[i + 2][j] == 'M')
+            {
+                // Check if we have an A to the right
+                if (j + 1 < grid[i].size() && grid[i + 1][j + 1] == 'A')
+                {
+                    // Check if we have an S to the right
+                    if ((j + 2 < grid[i].size() && grid[i][j + 2] == 'S') &&
+                        (j + 2 < grid[i].size() && grid[i + 2][j + 2] == 'S'))
+                    {
+                        count++;
+                    }
+                }
+
+                // Check if we have an A to the left
+                if (j - 1 >= 0 && grid[i + 1][j - 1] == 'A')
+                {
+                    // Check if we have an S to the left
+                    if ((j - 2 >= 0 && grid[i][j - 2] == 'S') &&
+                        (j - 2 >= 0 && grid[i + 2][j - 2] == 'S'))
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            // 4. Up
+            if (i - 2 >= 0 && grid[i - 2][j] == 'M')
+            {
+                // Check if we have an A to the right
+                if (j + 1 < grid[i].size() && grid[i - 1][j + 1] == 'A')
+                {
+                    // Check if we have an S to the right
+                    if ((j + 2 < grid[i].size() && grid[i][j + 2] == 'S') &&
+                        (j + 2 < grid[i].size() && grid[i - 2][j + 2] == 'S'))
+                    {
+                        count++;
+                    }
+                }
+
+                // Check if we have an A to the left
+                if (j - 1 >= 0 && grid[i - 1][j - 1] == 'A')
+                {
+                    // Check if we have an S to the left
+                    if ((j - 2 >= 0 && grid[i][j - 2] == 'S') &&
+                        (j - 2 >= 0 && grid[i - 2][j - 2] == 'S'))
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+    }
+
+    std::cout << "Count: " << count / 2 << std::endl;
     return 0;
 }
